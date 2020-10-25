e0059f856ab40600c82010f0e9b8fa62c58a4911
EllenSt264 <stafford.ellen@protonmail.com>
2020-10-25 14:02:21 +0000

    Add feat: player move sprite function
    Player can use arrow keys or WASD keys on their keyboard to move the sprite across the canvas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const player = {
         x: 100,               // player's horizontal position on the x axis
-        y: 600,               // player's vertical position on the y axis
+        y: 650,               // player's vertical position on the y axis
         width: 117.7857,          // width is calculated based on what spritesheet we use
         height: 113.125,         // height is calculated based on what spritesheet we use
         frameX: 0,          // the horizontal coordinate of frame we cut out from our sprite sheet
@@ -31,6 +31,7 @@ document.addEventListener("DOMContentLoaded", function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);   // clear entire canvas between every animation frame
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
         drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
+        movePlayer();
         requestAnimationFrame(animate);
     }
 
@@ -39,12 +40,33 @@ document.addEventListener("DOMContentLoaded", function() {
     // Key Event Listeners 
 
     window.addEventListener("keydown", function(e) {
-        keys[e.key] = true;     // when a key is pressed that key is added to the keys array
+        keys[e.keyCode] = true;     // when a key is pressed that key is added to the keys array
     });
 
     window.addEventListener("keyup", function(e) {
-        delete keys[e.key];     // when a key is released, that key is removed from the keys array
+        delete keys[e.keyCode];     // when a key is released, that key is removed from the keys array
     });
+
+    // Player move playerSprite
+
+    function movePlayer() {
+        // Up
+        if ((keys[38] || keys[87]) && player.y > 600) {
+            player.y -= player.speed;
+        }
+        // Down
+        if ((keys[40] || keys[83]) && player.y < canvas.height - player.height) {
+            player.y += player.speed;
+        }
+        // Right
+        if ((keys[39] || keys[68]) && player.x < canvas.width - player.width) {
+            player.x += player.speed;
+        }
+        // Left
+        if ((keys[37] || keys[65]) && player.x > 0) {
+            player.x -= player.speed;
+        }
+    }
 
     
 })
