8161a216d8be18ba12b9ffaa8773fbeae07494b3
EllenSt264 <stafford.ellen@protonmail.com>
2020-10-25 14:53:05 +0000

    Add feat: sprite animation frames for movement

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,11 +9,11 @@ document.addEventListener("DOMContentLoaded", function() {
     const player = {
         x: 100,               // player's horizontal position on the x axis
         y: 650,               // player's vertical position on the y axis
-        width: 117.7857,          // width is calculated based on what spritesheet we use
+        width: 103.0625,          // width is calculated based on what spritesheet we use
         height: 113.125,         // height is calculated based on what spritesheet we use
         frameX: 0,          // the horizontal coordinate of frame we cut out from our sprite sheet
         frameY: 0,          // the vertical coordinate of frame we cut out from our sprite sheet
-        speed: 9,           // how many pixels we move per frame
+        speed: 4,           // how many pixels we move per frame
         moving: false       // we will use this value to switch between standing and walking animation
     }
 
@@ -32,6 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
         drawSprite(playerSprite, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
         movePlayer();
+        handlePlayerFrame();
         requestAnimationFrame(animate);
     }
 
@@ -41,10 +42,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
     window.addEventListener("keydown", function(e) {
         keys[e.keyCode] = true;     // when a key is pressed that key is added to the keys array
+        player.moving = true;
     });
 
     window.addEventListener("keyup", function(e) {
         delete keys[e.keyCode];     // when a key is released, that key is removed from the keys array
+        player.moving = false;
     });
 
     // Player move playerSprite
@@ -53,20 +56,34 @@ document.addEventListener("DOMContentLoaded", function() {
         // Up
         if ((keys[38] || keys[87]) && player.y > 600) {
             player.y -= player.speed;
+            player.frameY = 0;
         }
         // Down
         if ((keys[40] || keys[83]) && player.y < canvas.height - player.height) {
             player.y += player.speed;
+            player.frameY = 6;
         }
         // Right
         if ((keys[39] || keys[68]) && player.x < canvas.width - player.width) {
             player.x += player.speed;
+            player.frameY = 3;
         }
         // Left
         if ((keys[37] || keys[65]) && player.x > 0) {
             player.x -= player.speed;
+            player.frameY = 3;
         }
     }
 
+    // To trigger sprite walking animation frames
     
+    function handlePlayerFrame() {
+        if (player.frameX < 7 && player.moving) {    // because our spritesheet has four collumns, starting from 0
+        player.frameX ++;
+        }
+        else {
+            player.frameX = 0;
+        }  
+    }
+
 })
